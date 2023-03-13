1fc890ebfc6fef69f10c6d375ea599487feb03df
Riyan Kurniawan <m.riyan.kurniawan@gmail.com>
2023-03-13 09:51:05 +0700

    First Commit

---
+++ tcp.js
@@ -0,0 +1,36 @@
+const net = require('net');
+
+var init = {
+    ClientID: null,
+    ServerDist: 1,
+};
+
+var dummy = {
+    SimID: '139928XXXXX',
+    SignalType: 'Locate',
+    DateTime: '2019-10-24 13:31:40'
+};
+
+
+function send_data(socket) {
+    let temp = JSON.stringify(dummy) + '# ';
+    socket.write(temp);
+}
+
+const server = net.createServer(function (socket) {
+    socket.write('Echo server\r\n');
+    socket.on('data', data => {
+        console.log("Receive any data")
+        console.log(data)
+        if (data.toString() === 'init') {
+            init.ClientID = socket.remoteAddress + ':' + socket.remotePort;
+            let temp = JSON.stringify(init) + '# ';
+            socket.write(temp);
+        }
+        else {
+
+        }
+    });
+});
+
+server.listen(8020, '127.0.0.1');
